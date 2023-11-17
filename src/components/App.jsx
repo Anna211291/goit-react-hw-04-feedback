be5034347bd07cf90965fa9961983349fc568b05
Anna211291 <Anjutka777@gmail.com>
2023-11-16 22:35:54 -0500

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,55 +1,53 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Section } from './Section/Section';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Notification } from './Notification/Notification';
 import { Statistic } from './Statistic/Statistic';
 import { Wrapper } from './GlobalStyle';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+  const { good, neutral, bad } = feedback;
+  const hendlerClick = option => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [option]: prevFeedback[option] + 1,
+    }));
   };
-  hendlerClick = option => {
-    this.setState(prevState => {
-      return { [option]: prevState[option] + 1 };
-    });
-  };
-  countTotalFeedback() {
-    const { good, neutral, bad } = this.state;
+
+  const countTotalFeedback = () => {
     const total = good + neutral + bad;
     return total;
-  }
-  countPositiveFeedbackPercentage() {
-    const totalFeedback = this.countTotalFeedback();
-    if (totalFeedback === 0) return 0;
-    return Math.round((this.state.good / totalFeedback) * 100);
-  }
-  render() {
-    const totalFeedback = this.countTotalFeedback();
-    return (
-      <Wrapper>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.hendlerClick}
+  };
+  const countPositiveFeedbackPercentage = () => {
+    const totalFeedback = countTotalFeedback();
+
+    return totalFeedback === 0
+      ? 0
+      : Math.round((feedback.good / totalFeedback) * 100);
+  };
+
+  return (
+    <Wrapper>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys(feedback)}
+          onLeaveFeedback={hendlerClick}
+        />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback <= 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistic
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
           />
-        </Section>
-        <Section title="Statistics">
-          {totalFeedback <= 0 ? (
-            <Notification message="There is no feedback" />
-          ) : (
-            <Statistic
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          )}
-        </Section>
-      </Wrapper>
-    );
-  }
-}
+        )}
+      </Section>
+    </Wrapper>
+  );
+};
